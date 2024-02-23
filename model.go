43fd23053ffeaf8438c42cdd6be94ab152c4107c
Kiril Yakymchuk <stbestich@gmail.com>
2024-02-23 16:45:30 +0200

    Get current time function

--- model.go
+++ model.go
@@ -5,3 +5,8 @@ import "time"
 type TimeResponse struct {
 	Time string `json:"time"`
 }
+
+func GetCurrentTime() TimeResponse {
+	currentTime := time.Now().Format(time.RFC3339)
+	return TimeResponse{ Time: currentTime }
+}
