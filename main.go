3f12968a66208bfecacac95e033ea5774f7ebda2
Cake2Rock <95484347+Cake2Rock@users.noreply.github.com>
2024-02-24 11:45:57 +0200

    Finished basic server structure

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 func main() {
     http.HandleFunc("/time", TimeHandler)
 
-port := ":8795"
-
+    port := ":8795"
+    if err := http.ListenAndServe(port, nil); err != nil {
+        panic(err)
+    }
 }
