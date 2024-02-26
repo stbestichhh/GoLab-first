52cae6851e6a01866edd5583c8cf9018812cd2ad
Kiril Yakymchuk <stbestich@gmail.com>
2024-02-26 12:38:19 +0200

    Fixed bug, formated code

--- handler.go
+++ handler.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http"
-    "time"
+	"encoding/json"
+	"net/http"
 )
 
 func TimeHandler(response http.ResponseWriter, request *http.Request) {
-    currentTime := GetCurrentTime()
-    jsonResponse, err := json.Marshal(currentTime)
-    if err != nil {
-        http.Error(response, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    response.Header().Set("Content-Type", "application/json")
-    response.WriteHeader(http.StatusOK)
-    response.Write(jsonResponse)
+	currentTime := GetCurrentTime()
+	jsonResponse, err := json.Marshal(currentTime)
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	response.Header().Set("Content-Type", "application/json")
+	response.WriteHeader(http.StatusOK)
+	response.Write(jsonResponse)
 }
