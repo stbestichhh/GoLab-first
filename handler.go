302dbc67a20f54041e8e73203182052128fd3c8d
anastasia-sl <anastasiaslynko2206@gmail.com>
2024-02-26 12:32:15 +0300

    Added TimeHandler function

--- handler.go
+++ handler.go
@@ -5,3 +5,15 @@ import (
     "net/http"
     "time"
 )
+
+func TimeHandler(response http.ResponseWriter, request *http.Request) {
+    currentTime := GetCurrentTime()
+    jsonResponse, err := json.Marshal(currentTime)
+    if err != nil {
+        http.Error(response, err.Error(), http.StatusInternalServerError)
+        return
+    }
+    response.Header().Set("Content-Type", "application/json")
+    response.WriteHeader(http.StatusOK)
+    response.Write(jsonResponse)
+}
